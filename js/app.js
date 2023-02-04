fa00310f430e55f36026cad91e59712a5e49d49a
Anthony P. Del Rosario <anthonypdr10@gmail.com>
2023-02-04 18:38:15 +0800

    Major update
    Added portfolio gallery, created a modal to display the gallery and program the gallery to function properly.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+function previewImage(img){
+    let source = img.src;
+    let index = source.indexOf("_");
+    let imageID = source.substring(index - 2,index);
+    if(imageID.slice(0,1) !== "0"){
+        setImage(imageID);
+    }else {
+        setImage(imageID.slice(1));
+    }
+}
+
+function setImage(imageID){
+    document.getElementById("previewImage").src = "/assets/websites/Sample "+imageID+".jpg";
+    $('#btnTrigger').click();
+}
