b8d14396f6bf578928941c672f2590efcaecb583
Anthony P. Del Rosario <anthonypdr10@gmail.com>
2023-02-05 01:15:11 +0800

    Portfolio Gallery Navigation
    Added Previous and Next button for the gallery

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-function previewImage(img){
-    let source = img.src;
+let imageID;
+
+function previewImage(image){
+    let source = image.src;
     let index = source.indexOf("_");
-    let imageID = source.substring(index - 2,index);
-    if(imageID.slice(0,1) !== "0"){
+    imageID = parseInt(source.substring(index - 2,index));
+
+    if(imageID.toString().slice(0,1) !== "0"){
         setImage(imageID);
     }else {
         setImage(imageID.slice(1));
     }
+
+    $('#btnTrigger').click();
 }
 
-function setImage(imageID){
-    document.getElementById("previewImage").src = "/assets/websites/Sample "+imageID+".jpg";
-    $('#btnTrigger').click();
+function goPrev(){
+    if(imageID == 1){
+        imageID = imageID + 50;
+        setImage(imageID);
+    }else {
+        imageID = imageID - 1;
+        setImage(imageID);
+    }
+}
+
+function goNext(){
+    if(imageID == 51){
+        imageID = imageID - 50;
+        setImage(imageID);
+    }else {
+        imageID = imageID + 1;
+        setImage(imageID);
+    }
+}
+
+function setImage(id){
+    document.getElementById("previewImage").src = "/assets/websites/Sample "+id+".jpg";
 }
